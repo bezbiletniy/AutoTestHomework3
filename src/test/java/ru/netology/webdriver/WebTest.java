f77075a290454006068c40f0e00ba272124c3130
bezbiletniy <a.borodin.ca@gmail.com>
2022-09-06 21:58:41 +0300

    Change chromedriver

--- src/test/java/ru/netology/webdriver/WebTest.java
+++ src/test/java/ru/netology/webdriver/WebTest.java
@@ -1,11 +1,13 @@
 package ru.netology.webdriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebTest {
 
@@ -13,12 +15,16 @@ public class WebTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver","driver/macOS/chromedriver");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
