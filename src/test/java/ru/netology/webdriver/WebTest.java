fb612d43079d4111cac18eec1b0bc4febe680a86
bezbiletniy <a.borodin.ca@gmail.com>
2022-09-06 20:44:28 +0300

    Change chromedriver

--- src/test/java/ru/netology/webdriver/WebTest.java
+++ src/test/java/ru/netology/webdriver/WebTest.java
@@ -1,8 +1,39 @@
 package ru.netology.webdriver;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver","driver/macOS/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearsDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldValidName() {
+        driver.get("http://localhost:9999");
+    }
+
+
+
 
 
 }
